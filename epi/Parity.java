9db242b983bae480d637dcbb3607ce4c0ff5f73a
Vlad R <vladcrash1147@yandex.ru>
2019-11-02 15:12:32 +0300

    solve 5.1

--- epi/Parity.java
+++ epi/Parity.java
@@ -4,8 +4,13 @@ import epi.test_framework.GenericTest;
 public class Parity {
   @EpiTest(testDataFile = "parity.tsv")
   public static short parity(long x) {
-    // TODO - you fill in here.
-    return 0;
+      short counter = 0;
+      while (x != 0) {
+          counter += x & 1;
+          x >>>= 1;
+      }
+
+      return (short) (counter % 2);
   }
 
   public static void main(String[] args) {
