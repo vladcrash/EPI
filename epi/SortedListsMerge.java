c40f2723cbb9e83955366713d04fc178e31cdd60
Vlad R <vladcrash1147@yandex.ru>
2019-11-06 23:31:44 +0300

    solve 8.1

--- epi/SortedListsMerge.java
+++ epi/SortedListsMerge.java
@@ -4,10 +4,22 @@ import epi.test_framework.GenericTest;
 public class SortedListsMerge {
   @EpiTest(testDataFile = "sorted_lists_merge.tsv")
   //@include
-  public static ListNode<Integer> mergeTwoSortedLists(ListNode<Integer> L1,
-                                                      ListNode<Integer> L2) {
-    // TODO - you fill in here.
-    return null;
+  public static ListNode<Integer> mergeTwoSortedLists(ListNode<Integer> p,
+                                                      ListNode<Integer> s) {
+    ListNode<Integer> dummy = new ListNode<>(0, null);
+    ListNode<Integer> head = dummy;
+    while (p != null && s != null) {
+        if (Integer.compare(p.data, s.data) < 0) {
+            head.next = p;
+            p = p.next;
+        } else {
+            head.next = s;
+            s = s.next;
+        }
+        head = head.next;
+    }
+    head.next = p != null ? p : s;
+    return dummy.next;
   }
 
   public static void main(String[] args) {
