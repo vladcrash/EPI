1d9e00c182760b764e1e9ac25831204a5a40c66a
Vlad R <vladcrash1147@yandex.ru>
2019-11-06 08:05:54 +0300

    solve 6.6

--- epi/BuyAndSellStock.java
+++ epi/BuyAndSellStock.java
@@ -5,8 +5,17 @@ import java.util.List;
 public class BuyAndSellStock {
   @EpiTest(testDataFile = "buy_and_sell_stock.tsv")
   public static double computeMaxProfit(List<Double> prices) {
-    // TODO - you fill in here.
-    return 0.0;
+    if (prices == null) {
+      return 0;
+    }
+
+    double max = 0;
+    double min = Double.MAX_VALUE;
+    for (int i = 0; i < prices.size(); i++) {
+        max = Math.max(max, prices.get(i) - min);
+        min = Math.min(min, prices.get(i));
+    }
+    return max;
   }
 
   public static void main(String[] args) {
