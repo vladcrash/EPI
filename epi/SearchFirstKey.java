683aa978b6c8484559955a23e35973836356f2b6
Vlad R <vladcrash1147@yandex.ru>
2019-11-09 23:51:05 +0300

    solve 12.1

--- epi/SearchFirstKey.java
+++ epi/SearchFirstKey.java
@@ -1,13 +1,29 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+
 import java.util.List;
 public class SearchFirstKey {
   @EpiTest(testDataFile = "search_first_key.tsv")
 
   public static int searchFirstOfK(List<Integer> A, int k) {
-    // TODO - you fill in here.
-    return 0;
+      if (A == null || A.isEmpty()) {
+          return -1;
+      }
+      
+      int left = 0;
+      int right = A.size() - 1;
+      while (left <= right) {
+          int mid = (right - left) / 2 + left;
+          if (A.get(mid) < k) {
+              left = mid + 1;
+          } else {
+              right = mid - 1;
+          }
+      }
+
+      return left < A.size() && A.get(left) == k ? left : -1;
   }
 
   public static void main(String[] args) {
