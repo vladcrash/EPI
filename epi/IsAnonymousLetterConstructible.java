0bcfb97f762c89ef6d7e400288a39ceba04c0e87
Vlad R <vladcrash1147@yandex.ru>
2019-11-10 01:44:38 +0300

    solve 13.2

--- epi/IsAnonymousLetterConstructible.java
+++ epi/IsAnonymousLetterConstructible.java
@@ -1,13 +1,37 @@
 package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class IsAnonymousLetterConstructible {
   @EpiTest(testDataFile = "is_anonymous_letter_constructible.tsv")
 
-  public static boolean isLetterConstructibleFromMagazine(String letterText,
-                                                          String magazineText) {
-    // TODO - you fill in here.
-    return true;
+  public static boolean isLetterConstructibleFromMagazine(String letter,
+                                                          String magazine) {
+    if (letter == null || magazine == null) {
+        return false;
+    }
+    Map<Character, Integer> frequency = new HashMap<>();
+    for (char c : letter.toCharArray()) {
+        frequency.put(c, frequency.getOrDefault(c, 0) + 1);
+    }
+
+    for (char c : magazine.toCharArray()) {
+        if (frequency.containsKey(c)) {
+            frequency.put(c, frequency.get(c) - 1);
+            if (frequency.get(c) == 0) {
+                frequency.remove(c);
+                if (frequency.isEmpty()) {
+                    break;
+                }
+            }
+
+        }
+    }
+
+    return frequency.isEmpty();
   }
 
   public static void main(String[] args) {
