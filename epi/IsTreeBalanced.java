17269e556811b4c8f19f46eeade5dd43f8d77ec3
Vlad R <vladcrash1147@yandex.ru>
2019-11-09 17:13:16 +0300

    solve 10.1

--- epi/IsTreeBalanced.java
+++ epi/IsTreeBalanced.java
@@ -5,9 +5,30 @@ public class IsTreeBalanced {
 
   @EpiTest(testDataFile = "is_tree_balanced.tsv")
 
-  public static boolean isBalanced(BinaryTreeNode<Integer> tree) {
-    // TODO - you fill in here.
-    return true;
+  public static boolean isBalanced(BinaryTreeNode<Integer> root) {
+    return isBalancedInternal(root) != Integer.MIN_VALUE;
+  }
+
+  private static int isBalancedInternal(BinaryTreeNode<Integer> root) {
+      if (root == null) {
+          return -1;
+      }
+
+      int left = isBalancedInternal(root.left);
+      if (left == Integer.MIN_VALUE) {
+          return Integer.MIN_VALUE;
+      }
+
+      int right = isBalancedInternal(root.right);
+      if (right == Integer.MIN_VALUE) {
+          return Integer.MIN_VALUE;
+      }
+
+      if (Math.abs(left - right) < 2) {
+          return Math.max(left, right) + 1;
+      } else {
+          return Integer.MIN_VALUE;
+      }
   }
 
   public static void main(String[] args) {
