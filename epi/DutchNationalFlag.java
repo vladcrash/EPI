33468ffbd7d33f12d37ac5d074c2ae46a34049e7
Vlad R <vladcrash1147@yandex.ru>
2019-11-10 15:32:42 +0300

    solve 6.1 by counting sort

--- epi/DutchNationalFlag.java
+++ epi/DutchNationalFlag.java
@@ -1,44 +1,33 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 import epi.test_framework.TestFailure;
 import epi.test_framework.TimedExecutor;
+
 import java.util.ArrayList;
 import java.util.List;
 public class DutchNationalFlag {
   public enum Color { RED, WHITE, BLUE }
 
   public static void dutchFlagPartition(int pivotIndex, List<Color> arr) {
-    Color pivot = arr.get(pivotIndex);
-    List<Color> less = new ArrayList<>();
-    List<Color> equal = new ArrayList<>();
-    List<Color> more = new ArrayList<>();
-
-    for (Color item : arr) {
-      if (item.ordinal() < pivot.ordinal()) {
-        less.add(item);
-      } else if (item.ordinal() == pivot.ordinal()) {
-        equal.add(item);
-      } else {
-        more.add(item);
+      if (arr == null || arr.isEmpty()) {
+          return;
       }
-    }
 
-    int j = 0;
-    for (Color item : less) {
-      arr.set(j, item);
-      j++;
-    }
-    for (Color item : equal) {
-      arr.set(j, item);
-      j++;
-    }
-    for (Color item : more) {
-      arr.set(j, item);
-      j++;
-    }
+      int[] count = new int[3];
+      for (Color color : arr) {
+          count[color.ordinal()]++;
+      }
 
+      int index = 0;
+      for (int i = 0; i < count.length; i++) {
+          while (count[i]-- > 0) {
+              arr.set(index++, Color.values()[i]);
+          }
+      }
   }
+
   @EpiTest(testDataFile = "dutch_national_flag.tsv")
   public static void dutchFlagPartitionWrapper(TimedExecutor executor,
                                                List<Integer> A, int pivotIdx)
