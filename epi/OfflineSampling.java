d92f961d81339c35df52a714667b3478a66be272
Vlad R <vladcrash1147@yandex.ru>
2019-11-17 20:06:01 +0300

    solve 6.11

--- epi/OfflineSampling.java
+++ epi/OfflineSampling.java
@@ -1,58 +1,75 @@
 package epi;
+
 import epi.test_framework.EpiTest;
-import epi.test_framework.RandomSequenceChecker;
 import epi.test_framework.GenericTest;
+import epi.test_framework.RandomSequenceChecker;
 import epi.test_framework.TimedExecutor;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+
+import java.util.*;
+
 public class OfflineSampling {
-  public static void randomSampling(int k, List<Integer> A) {
-    // TODO - you fill in here.
-    return;
-  }
-  private static boolean randomSamplingRunner(TimedExecutor executor, int k,
-                                              List<Integer> A)
-      throws Exception {
-    List<List<Integer>> results = new ArrayList<>();
+	public static void randomSampling(int k, List<Integer> arr) {
+		if (arr == null) {
+			return;
+		}
+		Set<Integer> result = new HashSet<>();
+		Random random = new Random();
 
-    executor.run(() -> {
-      for (int i = 0; i < 1000000; ++i) {
-        randomSampling(k, A);
-        results.add(new ArrayList<>(A.subList(0, k)));
-      }
-    });
+		for(int i = 0; i < k; i++) {
+			int next = random.nextInt(arr.size());
+			result.add(arr.get(next));
+			int last = arr.size() - 1;
+			Collections.swap(arr, next, last);
+			arr.remove(last);
+		}
 
-    int totalPossibleOutcomes =
-        RandomSequenceChecker.binomialCoefficient(A.size(), k);
-    Collections.sort(A);
-    List<List<Integer>> combinations = new ArrayList<>();
-    for (int i = 0; i < RandomSequenceChecker.binomialCoefficient(A.size(), k);
-         ++i) {
-      combinations.add(
-          RandomSequenceChecker.computeCombinationIdx(A, A.size(), k, i));
-    }
-    List<Integer> sequence = new ArrayList<>();
-    for (List<Integer> result : results) {
-      Collections.sort(result);
-      sequence.add(combinations.indexOf(result));
-    }
-    return RandomSequenceChecker.checkSequenceIsUniformlyRandom(
-        sequence, totalPossibleOutcomes, 0.01);
-  }
+		arr.clear();
+		arr.addAll(result);
+	}
 
-  @EpiTest(testDataFile = "offline_sampling.tsv")
-  public static void randomSamplingWrapper(TimedExecutor executor, int k,
-                                           List<Integer> A) throws Exception {
-    RandomSequenceChecker.runFuncWithRetries(
-        () -> randomSamplingRunner(executor, k, A));
-  }
+	private static boolean randomSamplingRunner(TimedExecutor executor, int k,
+												List<Integer> A)
+			throws Exception {
+		List<List<Integer>> results = new ArrayList<>();
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "OfflineSampling.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+		executor.run(() -> {
+			for (int i = 0; i < 1000000; ++i) {
+				randomSampling(k, A);
+				results.add(new ArrayList<>(A.subList(0, k)));
+			}
+		});
+
+		int totalPossibleOutcomes =
+				RandomSequenceChecker.binomialCoefficient(A.size(), k);
+		Collections.sort(A);
+		List<List<Integer>> combinations = new ArrayList<>();
+		for (int i = 0; i < RandomSequenceChecker.binomialCoefficient(A.size(), k);
+			 ++i) {
+			combinations.add(
+					RandomSequenceChecker.computeCombinationIdx(A, A.size(), k, i));
+		}
+		List<Integer> sequence = new ArrayList<>();
+		for (List<Integer> result : results) {
+			Collections.sort(result);
+			sequence.add(combinations.indexOf(result));
+		}
+		return RandomSequenceChecker.checkSequenceIsUniformlyRandom(
+				sequence, totalPossibleOutcomes, 0.01);
+	}
+
+	@EpiTest(testDataFile = "offline_sampling.tsv")
+	public static void randomSamplingWrapper(TimedExecutor executor, int k,
+											 List<Integer> A) throws Exception {
+		RandomSequenceChecker.runFuncWithRetries(
+				() -> randomSamplingRunner(executor, k, A));
+	}
+
+	public static void main(String[] args) {
+		System.exit(
+				GenericTest
+						.runFromAnnotations(args, "OfflineSampling.java",
+								new Object() {
+								}.getClass().getEnclosingClass())
+						.ordinal());
+	}
 }
