c5e4c9bebb93b063148881fb6db281875b5226e7
Vlad R <vladcrash1147@yandex.ru>
2019-11-17 16:32:08 +0300

    solve 5.7

--- epi/PowerXY.java
+++ epi/PowerXY.java
@@ -1,18 +1,45 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
-public class PowerXY {
-  @EpiTest(testDataFile = "power_x_y.tsv")
-  public static double power(double x, int y) {
-    // TODO - you fill in here.
-    return 0.0;
-  }
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "PowerXY.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+import java.util.HashMap;
+import java.util.Map;
+
+public class PowerXY {
+	@EpiTest(testDataFile = "power_x_y.tsv")
+	public static double power(double x, int y) {
+		if (y < 0) {
+			y *= -1;
+			x = 1.0 / x;
+		}
+
+		Map<Integer, Double> values = new HashMap<>();
+		double result = 1.0;
+		int i = 2;
+		values.put(1, x);
+		while (i <= y) {
+			values.put(i, values.get(i / 2) * values.get(i / 2));
+			i *= 2;
+		}
+
+		while (y != 0) {
+			while (y % i == y) {
+				i /= 2;
+			}
+			y -= i;
+			result *= values.get(i);
+		}
+
+		return result;
+	}
+
+	public static void main(String[] args) {
+		System.exit(
+				GenericTest
+						.runFromAnnotations(args, "PowerXY.java",
+								new Object() {
+								}.getClass().getEnclosingClass())
+						.ordinal());
+	}
 }
