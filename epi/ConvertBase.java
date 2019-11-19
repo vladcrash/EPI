f19461058c88b555b71a03b6f8375fe7948a948c
Vlad R <vladcrash1147@yandex.ru>
2019-11-19 23:31:59 +0300

    solve 7.2

--- epi/ConvertBase.java
+++ epi/ConvertBase.java
@@ -1,19 +1,28 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+
 public class ConvertBase {
-  @EpiTest(testDataFile = "convert_base.tsv")
+	@EpiTest(testDataFile = "convert_base.tsv")
 
-  public static String convertBase(String numAsString, int b1, int b2) {
-    // TODO - you fill in here.
-    return "";
-  }
+	public static String convertBase(String num, int b1, int b2) {
+		if (num == null || num.isEmpty()) {
+			return null;
+		}
+		if (num.equals("-0")) {
+			return num;
+		}
+		int numInBaseTen = Integer.parseInt(num, b1);
+		return Integer.toString(numInBaseTen, b2).toUpperCase();
+	}
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "ConvertBase.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+	public static void main(String[] args) {
+		System.exit(
+				GenericTest
+						.runFromAnnotations(args, "ConvertBase.java",
+								new Object() {
+								}.getClass().getEnclosingClass())
+						.ordinal());
+	}
 }
